65f2f5fe0ea662cd872f6b1b4d993728cd7f85d5
mistcraft0 <mistcraft0@gmail.com>
2026-01-17 19:28:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,10 @@
 function copyIP() {
-    // Twoje IP
-    const ipText = "Mistcraft.pl"; 
-    
+    const ipText = "MistCraft.pl"; 
     navigator.clipboard.writeText(ipText).then(() => {
         const confirmation = document.getElementById("copy-confirmation");
-        const ipBox = document.querySelector(".server-ip-box");
-        
         confirmation.style.display = "block";
-        ipBox.style.borderColor = "#00ff00"; // Zielony błysk przy sukcesie
-        
         setTimeout(() => {
             confirmation.style.display = "none";
-            ipBox.style.borderColor = "#00B4DB"; // Powrót do niebieskiego
-        }, 2000);
-    }).catch(err => {
-        console.error('Błąd kopiowania: ', err);
-        alert("Nie udało się skopiować IP automatycznie. Adres to: Mistcraft.pl");
+        }, 3000);
     });
 }
